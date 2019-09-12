b39ab3580e97bd82e33ad020cfbbfe7c6c2656cd
katapi2000 <katapi2000@gmail.com>
2019-09-12 22:38:16 +0900

    [new] member紹介

---
+++ src/components/member.vue
@@ -0,0 +1,86 @@
+<template lang="pug">
+    div
+        .container
+            .filters
+                span.filter(v-bind:class="{ active: currentFilter === 'ALL' }" v-on:click="setFilter('ALL')") ALL
+                span.filter(v-bind:class="{ active: currentFilter === 'DJ' }" v-on:click="setFilter('DJ')") DJ
+                span.filter(v-bind:class="{ active: currentFilter === 'DTM' }" v-on:click="setFilter('DTM')") DTM
+
+        transition-group.members(name="members")
+            .member(v-if="currentFilter === member.category || currentFilter === 'ALL'" v-bind:key="member.name" v-for="member in members")
+                .member-image-wrapper
+                    img.member-image(v-bind:src="member.image")
+                .member-name
+                    span.member-name {{member.name}}
+                .member-category
+                    p {{member.category}}
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            currentFilter: 'ALL',
+            members: [
+                {name: "Massu", image: require("../assets/img/member/massu.jpg"), category: 'DJ'},
+                {name: "Ma", image: require("../assets/img/member/massu.jpg"), category: 'DTM'},
+                {name: "Mas", image: require("../assets/img/member/massu.jpg"), category: 'DJ'},
+                {name: "Mass", image: require("../assets/img/member/massu.jpg"), category: 'DJ'},
+                {name: "Msu", image: require("../assets/img/member/massu.jpg"), category: 'DTM'},
+                {name: "Mu", image: require("../assets/img/member/massu.jpg"), category: 'DJ'},
+                {name: "M", image: require("../assets/img/member/massu.jpg"), category: 'DJ'},
+            ]
+        }
+	},
+	methods: {
+		setFilter: function(filter) {
+			this.currentFilter = filter;
+		}
+    }
+}
+</script>
+
+<style>
+.container {
+	display:flex;
+	flex-direction:column;
+	justify-content:center;
+	align-items:center;
+}
+
+.member-name {
+    font-size: 20px;
+}
+
+.filters {
+    font-size: 24px;
+}
+
+.filter {
+	font-family:arial;
+	padding: 6px 6px;
+	cursor:pointer;
+	border-radius: 6px;
+	transition: all 0.35s;
+    margin: 0px 15px;
+}
+
+.member-image {
+    width: 200px;
+}
+
+.members {
+    margin-top:25px;
+	display:flex;
+	flex-wrap:wrap;
+	justify-content:center;
+}
+
+.member {
+    margin: 0px 10px;
+    margin-bottom: 50px;
+	box-shadow:0px 2px 8px lightgrey;
+	flex-direction:column;
+	align-items:center;
+}
+</style>
