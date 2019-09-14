82942f6c1019f434f685f4e13e9155d25507b907
katapi2000 <katapi2000@gmail.com>
2019-09-14 16:23:22 +0900

    [update] components追加

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,30 @@
 <template lang="pug">
 div
 	top/
+	h1 ABOUT
+	about/
+	h1 GUEST
+	guest/
+	h1 MEMBER
 	member/
+	h1 timetable
+	timetable/
 </template>
 
 <script>
 import top from '../components/top.vue'
+import about from '../components/about.vue'
+import guest from '../components/guest.vue'
 import member from '../components/member.vue'
+import timetable from '../components/timetable.vue'
 
 export default {
 	components: {
 		top,
-		member
+		about,
+		guest,
+		member,
+		timetable,
 	},
 };
 </script>
