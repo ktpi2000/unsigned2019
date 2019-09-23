7a6e1978abe7efa9084a28c22deed0f75063e63f
katapi2000 <katapi2000@gmail.com>
2019-09-24 02:28:59 +0900

    [add] about追記

--- src/components/about.vue
+++ src/components/about.vue
@@ -1,3 +1,18 @@
 <template lang="pug">
-    p unsignedはスゴイイベントです
+div
+    p 「アニソンとダンスミュージックが好き！」
+    |「思いっきり踊ってストレス発散したい！」
+    br
+    br
+    | 教室をDJブースに改造して、みんなで機材を持ち込んでフロアを熱くしてきたUnsigned。
+    br
+    | 今年でついに8年目！
+    br
+    br
+    | AINISONsideは、アニソン原曲を中心にRemix、ボカロ等、
+    br
+    | DANCEsideは、BPM速い音楽を中心に様々なジャンルでDJしていくぞ！
+    br
+    br
+    | 音研DJクルーの躍動感あふれるDJプレイで最高になろう！
 </template>
