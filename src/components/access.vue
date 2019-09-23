34d119d3ffb536bab9f468142603f54004afdacc
katapi2000 <katapi2000@gmail.com>
2019-09-24 00:17:20 +0900

    [add] 画像追加

--- src/components/access.vue
+++ src/components/access.vue
@@ -9,8 +9,9 @@
             p 栃木県小山市中久喜771
             br
             a.button.is-info(target="_blank" href="https://www.google.com/maps/place/%E5%9B%BD%E7%AB%8B%E5%B0%8F%E5%B1%B1%E5%B7%A5%E6%A5%AD%E9%AB%98%E7%AD%89%E5%B0%82%E9%96%80%E5%AD%A6%E6%A0%A1/@36.3186853,139.8399723,17z/data=!3m1!4b1!4m5!3m4!1s0x601f4e17aa8ac303:0xffd4bb31378636da!8m2!3d36.318681!4d139.842161") Google map
-        .column.is-6
+        .column.is-5
             h2 電車でお越しの方
+            img(src="@/assets/img/traffic-map.jpg" alt="traffic-map")
             h2 バスのご案内
             h2 お車でお越しの方
 </template>
