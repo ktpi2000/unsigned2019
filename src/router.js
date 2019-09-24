f080c92ba09fc6ecfcc70180d2039090e3b0e81a
katapi2000 <katapi2000@gmail.com>
2019-09-24 13:58:41 +0900

    [add] 404

--- src/router.js
+++ src/router.js
@@ -13,12 +13,9 @@ export default new Router({
       component: () => import('./views/Home.vue')
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '*',
+      name: 'Not Found',
+      component: () => import('./views/404.vue')
     }
   ]
 })
