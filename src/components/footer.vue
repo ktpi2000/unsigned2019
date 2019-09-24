0379596388fc3b57fc7034ed2d610122cf2f2726
katapi2000 <katapi2000@gmail.com>
2019-09-24 22:00:57 +0900

    [edit] footer弄ってた

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,7 +1,12 @@
 <template lang="pug">
 .footer
-    h1 Unsigned2019
-    h2 #un_8th
+    .columns.is-centered
+        .column.is-3
+            img(src="../assets/img/member/un_8th_icon.png" width="50px")
+            h1.is-size-3 Unsigned2019
+            
+        .column.is-3
+            h2.is-size-4 #un_8th
     small &copy; Unsigned2019
 </template>
 
@@ -10,5 +15,8 @@
     margin-top: 150px;
     background: black;
     color: white;
+}
+img {
+    float: left;
 }
 </style>
