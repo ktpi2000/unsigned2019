b2251bff795b60a9d36822848d641a6d8efa2cd9
katapi2000 <katapi2000@gmail.com>
2019-09-24 17:08:41 +0900

    [change] font-size

--- src/components/access.vue
+++ src/components/access.vue
@@ -1,16 +1,18 @@
 <template lang="pug">
 div
-    h1 会場
+    h1.is-size-4 会場
     p 小山工業高等専門学校
     p 専攻科棟4F 多目的ホール
     br
-    h1 住所
+    h1.is-size-4 住所
     p 栃木県小山市中久喜771
     br
     a.button.is-info(target="_blank" href="https://www.google.com/maps/place/%E5%9B%BD%E7%AB%8B%E5%B0%8F%E5%B1%B1%E5%B7%A5%E6%A5%AD%E9%AB%98%E7%AD%89%E5%B0%82%E9%96%80%E5%AD%A6%E6%A0%A1/@36.3186853,139.8399723,17z/data=!3m1!4b1!4m5!3m4!1s0x601f4e17aa8ac303:0xffd4bb31378636da!8m2!3d36.318681!4d139.842161") Google map
+    br
+    br
     .columns.is-centered
         .column.is-5
-            h2 電車でお越しの方
+            h2.is-size-4 ◇電車でお越しの方
             img(src="@/assets/img/traffic-map.jpg" alt="traffic-map")
             h2 バスのご案内
             p.platform 乗り場: 小山駅東口バスのりば
@@ -31,7 +33,7 @@ div
                             | 城東中久喜線 高専正門で下車 
                             a.button.is-info.is-small(href="https://www.city.oyama.tochigi.jp/uploaded/attachment/1818.pdf" target="_blank") 時刻表
         .column.is-5
-            h2 お車でお越しの方
+            h2.is-size-4 ◇お車でお越しの方
             img(src="@/assets/img/parking.jpg" alt="parking-map")
             .content
                 ul   
@@ -46,5 +48,8 @@ h2 {
 }
 li,.fare,.platform {
     text-align: left;
+}
+p, li {
+    font-size: 1.25rem;
 }
 </style>
