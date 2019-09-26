caabb44acff3bf4bcb44a4ee163665e71af9d77d
katapi2000 <katapi2000@gmail.com>
2019-09-26 22:25:06 +0900

    [change] パスの書き方

--- src/components/DJ/top.vue
+++ src/components/DJ/top.vue
@@ -4,13 +4,13 @@ body
     .top-slideshow-wrapper
       ul#slideshow
         li
-          img.topimg(src="../../assets/img/top001.jpg" alt="top img1")
+          img.topimg(src="@/assets/img/top001.jpg" alt="top img1")
         li
-          img.topimg(src="../../assets/img/top002.jpg" alt="top img2")
+          img.topimg(src="@/assets/img/top002.jpg" alt="top img2")
         li
-          img.topimg(src="../../assets/img/top003.jpg" alt="top img3")
+          img.topimg(src="@/assets/img/top003.jpg" alt="top img3")
         li
-          img.topimg(src="../../assets/img/top005.jpg" alt="top img5")
+          img.topimg(src="@/assets/img/top005.jpg" alt="top img5")
   figcaption.title
     .main-title Unsiged2019
     .sub-text 
