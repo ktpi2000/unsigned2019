ca23072ede89fddff0ba9530402d8923ddb10d9c
katapi2000 <katapi2000@gmail.com>
2019-09-26 16:14:22 +0900

    [add] vue-analytics

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
 import VueBlockReveal from 'vue-block-reveal'
+import VueAnalytics from 'vue-analytics'
 
 Vue.use(Buefy)
 
@@ -12,6 +13,11 @@ Vue.config.productionTip = false
 Vue.use(VueBlockReveal, {
   direction: 'lr',
   bgcolor: '#9b003f'
+});
+
+Vue.use(VueAnalytics, {
+  id: 'UA-126717108-2',
+  router
 });
 
 new Vue({
