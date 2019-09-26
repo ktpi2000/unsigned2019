2521fda154870f3e4ff870e914aff421543fffca
katapi2000 <katapi2000@gmail.com>
2019-09-27 00:55:00 +0900

    [update] いい感じのfooterできた

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,12 +1,16 @@
 <template lang="pug">
 .Footer
     .columns.is-centered
+        .column.is-3
+            img(src="../assets/img/hashtag.png" alt="hashtag" width="200")
+            h1.is-size-5
+                | follow us 
+                b-button(type="is-info" size="is-small" rounded) @OyNCT_onken
+
         .column.is-3.un
             img(src="../assets/img/member/un_8th_icon.png" width="50px" alt="logo")
             h1.is-size-3 Unsigned2019
-            
-        .column.is-3
-            h2.is-size-4 #un_8th
+        
     small &copy; Unsigned2019
 </template>
 
