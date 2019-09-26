00aa6245bdeb8efedd37d4c4dc7ed4e7b7de7b52
katapi2000 <katapi2000@gmail.com>
2019-09-27 00:04:53 +0900

    [move] footer

--- src/App.vue
+++ src/App.vue
@@ -10,14 +10,17 @@
     <transition mode="out-in">
       <router-view/>
     </transition>
+    <Footer/>
   </div>
 </template>
 
 <script>
 import Header from '@/components/header.vue'
+import Footer from '@/components/footer.vue'
 export default {
   components: {
     Header,
+    Footer,
   }
 }
 </script>
