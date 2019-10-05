acc990e30721cec5995d90c0ebb35e73b955a6c3
katapi2000 <katapi2000@gmail.com>
2019-10-05 17:05:39 +0900

    [clean] 共通cssの整理

--- src/views/TT.vue
+++ src/views/TT.vue
@@ -1,23 +1,30 @@
 <template lang="pug">
 .tt
     .main-title(v-block-reveal="") TIME TABLE
-    timetable/
+    anison_tt/
+    dance_tt/
 </template>
 
 <script>
-import timetable from '../../src/components/DJ/timetable.vue'
+import anison_tt from '../../src/components/DJ/anison_tt.vue'
+import dance_tt from '../../src/components/DJ/dance_tt.vue'
 export default {
     components: {
-        timetable,
+        anison_tt,
+        dance_tt,
     }
 }
 </script>
 
 <style scoped>
+@import url("../assets/style/timetable.css");
 .main-title {
 	font-size: 50px;
 	margin: 30px 0 10px 0;
 	padding: 5px 0;
     color: #9b003f;
+}
+.tt {
+    margin-bottom: 60px;
 }
 </style>
