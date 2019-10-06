c6aaa635ee78d1a7dd45450a091067b962dd0ce6
katapi2000 <katapi2000@gmail.com>
2019-10-06 17:52:29 +0900

    [add] 時刻表データ

--- src/components/Access/freebus.vue
+++ src/components/Access/freebus.vue
@@ -1,14 +1,23 @@
 <template lang="pug">
 div
-  .modal-card(style="width: 600")
+  .modal-card(width="300")
     header.modal-card-head
       p.modal-card-title 無料シャトルバス
     section.modal-card-body
-      table
-        tbody
-            tr(v-for="freebus in freebuss" :key="freebus.id")
-                th(align="center") {{ freebus.hour}}
-                td {{ freebus.min}}
+      b-tabs(type="is-toggle-rounded" expanded size="is-medium")
+        b-tab-item(label="小山駅▷小山高専")
+          table
+            tbody
+                tr(v-for="for_kosen in for_kosens" :key="for_kosen.id")
+                    th(align="center") {{ for_kosen.hour}}
+                    td {{ for_kosen.min}}
+        b-tab-item(label="小山高専▷小山駅")
+          table
+            tbody
+                tr(v-for="for_sta in for_stas" :key="for_sta.id")
+                    th(align="center") {{ for_sta.hour}}
+                    td {{ for_sta.min}}
+      
 
     footer.modal-card-foot
       button.button.is-rounded.is-medium( @click="$parent.close()") close
@@ -18,12 +27,98 @@ div
 export default {
     data: function() {
         return {
-            freebuss: [
+            for_kosens: [
                 {
-                    hour: "11:00",
-                    min: "yakiya",
+                    hour: "9",
+                    min: "55",
+                },
+                {
+                    hour: "10",
+                    min: "10　45",
+                },
+                {
+                    hour: "11",
+                    min: "00　35　50",
+                },
+                {
+                    hour: "12",
+                    min: "25　40",
+                },
+                {
+                    hour: "13",
+                    min: "45",
+                },
+                {
+                    hour: "14",
+                    min: "00　35　50",
+                },
+                {
+                    hour: "15",
+                    min: "25　40",
+                },
+                {
+                    hour: "16",
+                    min: "15　30",
+                },
+                {
+                    hour: "17",
+                    min: "25　40",
+                },
+                {
+                    hour: "18",
+                    min: "15　30",
+                },
+                {
+                    hour: "19",
+                    min: "05　20",
                 },
             ],
+            for_stas: [
+                {
+                    hour: "9",
+                    min: "30　45",
+                },
+                {
+                    hour: "10",
+                    min: "20　35",
+                },
+                {
+                    hour: "11",
+                    min: "10　25",
+                },
+                {
+                    hour: "12",
+                    min: "00　15",
+                },
+                {
+                    hour: "13",
+                    min: "20　35",
+                },
+                {
+                    hour: "14",
+                    min: "10　25",
+                },
+                {
+                    hour: "15",
+                    min: "00　15　50",
+                },
+                {
+                    hour: "16",
+                    min: "05",
+                },
+                {
+                    hour: "17",
+                    min: "00　15　50",
+                },
+                {
+                    hour: "18",
+                    min: "05　40　55",
+                },
+                {
+                    hour: "19",
+                    min: "30　45",
+                },
+            ]
         }
     }
 }
--- src/components/Access/oyamabus.vue
+++ src/components/Access/oyamabus.vue
@@ -1,29 +1,153 @@
 <template lang="pug">
 div
-  .modal-card(style="width: 600")
+  .modal-card(width="300")
     header.modal-card-head
       p.modal-card-title おーバス
     section.modal-card-body
-      table
-        tbody
-            tr(v-for="oyamabus in oyamabuss" :key="oyamabus.id")
-                th(align="center") {{ oyamabus.hour}}
-                td {{ oyamabus.min}}
+      b-tabs(type="is-toggle-rounded" expanded size="is-medium")
+        b-tab-item(label="小山駅▷小山高専")
+          table
+            tbody
+                tr(v-for="for_kosen in for_kosens" :key="for_kosen.id")
+                    th(align="center") {{ for_kosen.hour}}
+                    td {{ for_kosen.min}}
+        b-tab-item(label="小山高専▷小山駅")
+          table
+            tbody
+                tr(v-for="for_sta in for_stas" :key="for_sta.id")
+                    th(align="center") {{ for_sta.hour}}
+                    td {{ for_sta.min}}
+      
 
     footer.modal-card-foot
       button.button.is-rounded.is-medium( @click="$parent.close()") close
+      p 無印:城東中久喜線 / ○印:高岳線
 </template>
 
 <script>
 export default {
     data: function() {
         return {
-            oyamabuss: [
+            for_kosens: [
                 {
-                    hour: "11:00",
-                    min: "yakiya",
+                    hour: "6",
+                    min: "20",
+                },
+                {
+                    hour: "7",
+                    min: "40　　57○",
+                },
+                {
+                    hour: "8",
+                    min: "50　　50○",
+                },
+                {
+                    hour: "9",
+                    min: "40○　50",
+                },
+                {
+                    hour: "10",
+                    min: "30○　50",
+                },
+                {
+                    hour: "11",
+                    min: "30○　50",
+                },
+                {
+                    hour: "13",
+                    min: "00○　30",
+                },
+                {
+                    hour: "14",
+                    min: "00○　30",
+                },
+                {
+                    hour: "15",
+                    min: "00○　30",
+                },
+                {
+                    hour: "16",
+                    min: "00○　40",
+                },
+                {
+                    hour: "17",
+                    min: "00○　40",
+                },
+                {
+                    hour: "18",
+                    min: "00○　40",
+                },
+                {
+                    hour: "19",
+                    min: "00○　40",
+                },
+                {
+                    hour: "20",
+                    min: "00○　40",
                 },
             ],
+            for_stas: [
+                {
+                    hour: "6",
+                    min: "45",
+                },
+                {
+                    hour: "7",
+                    min: "24○",
+                },
+                {
+                    hour: "8",
+                    min: "05　　24○",
+                },
+                {
+                    hour: "9",
+                    min: "14○　15",
+                },
+                {
+                    hour: "10",
+                    min: "04○　15　54○",
+                },
+                {
+                    hour: "11",
+                    min: "15　　54○",
+                },
+                {
+                    hour: "12",
+                    min: "15",
+                },
+                {
+                    hour: "13",
+                    min: "24○　55",
+                },
+                {
+                    hour: "14",
+                    min: "24○　55",
+                },
+                {
+                    hour: "15",
+                    min: "24○　55",
+                },
+                {
+                    hour: "16",
+                    min: "24○",
+                },
+                {
+                    hour: "17",
+                    min: "05　　34○",
+                },
+                {
+                    hour: "18",
+                    min: "05　　34○",
+                },
+                {
+                    hour: "19",
+                    min: "05　　34○",
+                },
+                {
+                    hour: "20",
+                    min: "05　　34○",
+                },
+            ]
         }
     }
 }
