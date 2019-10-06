a6cc02d8329de95f169501d3c01a99b310d4a625
katapi2000 <katapi2000@gmail.com>
2019-10-06 16:57:03 +0900

    [add] バス時刻表

--- src/components/Access/freebus.vue
+++ src/components/Access/freebus.vue
@@ -2,9 +2,13 @@
 div
   .modal-card(style="width: 600")
     header.modal-card-head
-      p.modal-card-title hoge
+      p.modal-card-title 無料シャトルバス
     section.modal-card-body
-      p head
+      table
+        tbody
+            tr(v-for="freebus in freebuss" :key="freebus.id")
+                th(align="center") {{ freebus.hour}}
+                td {{ freebus.min}}
 
     footer.modal-card-foot
       button.button.is-rounded.is-medium( @click="$parent.close()") close
@@ -12,9 +16,19 @@ div
 
 <script>
 export default {
+    data: function() {
+        return {
+            freebuss: [
+                {
+                    hour: "11:00",
+                    min: "yakiya",
+                },
+            ],
+        }
+    }
 }
 </script>
 
 <style scoped>
-
+@import url("../../assets/style/bus_timetable.css");
 </style>
--- src/components/Access/oyamabus.vue
+++ src/components/Access/oyamabus.vue
@@ -2,9 +2,13 @@
 div
   .modal-card(style="width: 600")
     header.modal-card-head
-      p.modal-card-title hogeee
+      p.modal-card-title おーバス
     section.modal-card-body
-      p head
+      table
+        tbody
+            tr(v-for="oyamabus in oyamabuss" :key="oyamabus.id")
+                th(align="center") {{ oyamabus.hour}}
+                td {{ oyamabus.min}}
 
     footer.modal-card-foot
       button.button.is-rounded.is-medium( @click="$parent.close()") close
@@ -12,9 +16,19 @@ div
 
 <script>
 export default {
+    data: function() {
+        return {
+            oyamabuss: [
+                {
+                    hour: "11:00",
+                    min: "yakiya",
+                },
+            ],
+        }
+    }
 }
 </script>
 
 <style scoped>
-
+@import url("../../assets/style/bus_timetable.css");
 </style>
