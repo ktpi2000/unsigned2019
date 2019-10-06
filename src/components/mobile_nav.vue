4a0c44cb551050b31a60ec4cbd3f0085b726e305
katapi2000 <katapi2000@gmail.com>
2019-10-06 22:00:25 +0900

    [fix] 色切替修正

--- src/components/mobile_nav.vue
+++ src/components/mobile_nav.vue
@@ -2,41 +2,22 @@
 .nav
     ul
         li.menu.is-active
-            router-link(to="/")
+            router-link(to="/" exact-active-class="active")
                 img(src="@/assets/icon/compact-disc-solid.svg")
                 p DJ
         li.menu
-            router-link(to="/tt")
+            router-link(to="/tt" exact-active-class="active")
                 img(src="@/assets/icon/border-all-solid.svg")
                 p TT
         li.menu
-            router-link(to="/dtm")
+            router-link(to="/dtm" exact-active-class="active")
                 img(src="@/assets/icon/headphones-alt-solid.svg")
                 p DTM
         li.menu
-            router-link(to="/access")
+            router-link(to="/access" exact-active-class="active")
                 img(src="@/assets/icon/map-marker-alt-solid.svg")
                 p ACCESS
 </template>
-
-<script>
-export default {
-    mounted: function() {
-        document.addEventListener('DOMContentLoaded', function(){
-            // タブに対してクリックイベントを適用
-            const menus = document.getElementsByClassName('menu');
-                for(let i = 0; i < menus.length; i++) {
-                    menus[i].addEventListener('click', colorSwitch, false);
-                }
-
-            function colorSwitch(){
-                document.getElementsByClassName('is-active')[0].classList.remove('is-active');
-                this.classList.add('is-active');
-            }
-        });
-    }
-}
-</script>
 
 <style scoped>
 .nav {
@@ -57,7 +38,7 @@ a {
     padding-top: 3px;
     color: black;
 }
-.is-active {
+.active {
     background: #9b003f;
     transition: all 0.2s ease-out;
 }
