e821c34594de58d9b4acf8b7caf0a2363bdedc54
katapi2000 <katapi2000@gmail.com>
2019-10-28 23:34:21 +0900

    [update] コンテンツ更新

--- src/components/DTM/new.vue
+++ src/components/DTM/new.vue
@@ -6,17 +6,25 @@
             //.subtitle DOWNLOAD
         .column.is-3
             .title higher
-            //.content
+            .content
                 ol
-                    li unsigned
-                    li unsigned
-                    li unsigned
-            h1.title 現在曲を製作中！
+                    li Night In Route 228 - neya
+                    li Pirate air ship in Azure - 雪華
+                    li 浪花NaniwaFunk - マイシパ95
+                    li Shadow cast on - AOhari
+                    li 夕映えの渚 - 雪華
+                    li Boarding the Halberd Arranged - neya
+                    li カフェ太郎 くっとこうた - あみゅー
+                    li 快晴Snowytown - AOhari
+                    li Treasure in Sky island - 雪華
+                    li 夕飛 - AOhari
+            a.button.is-danger.is-large.is-rounded(href="https://dtmkosen-1-1.tumblr.com/" target="_blank") Free DL →
             
 </template>
 
 <style scoped>
 li, .tl {
     text-align: left;
+    font-weight: 600;
 }
 </style>
