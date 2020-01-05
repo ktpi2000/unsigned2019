759071e7426f989743e07a205648fd88a0f10e38
ktpi2000 <katapi2000@gmail.com>
2020-01-06 01:21:59 +0900

    #10 [update] swiperに書き換え / css整理

--- src/components/DJ/top.vue
+++ src/components/DJ/top.vue
@@ -1,18 +1,17 @@
 <template lang="pug">
 body
-  main
-    .top-slideshow-wrapper
-      ul#slideshow
-        li
-          img.topimg(src="@/assets/img/top001.jpg" alt="top img1")
-        li
-          img.topimg(src="@/assets/img/top002.jpg" alt="top img2")
-        li
-          img.topimg(src="@/assets/img/top003.jpg" alt="top img3")
-        li
-          img.topimg(src="@/assets/img/top004.jpg" alt="top img4")
-        li
-          img.topimg(src="@/assets/img/top005.jpg" alt="top img5")
+  .swiper-container
+    swiper(:options="swiperOption")
+      swiper-slide
+        img(src="@/assets/img/top001.jpg" alt="top img1")
+      swiper-slide
+        img(src="@/assets/img/top002.jpg" alt="top img2")
+      swiper-slide
+        img(src="@/assets/img/top003.jpg" alt="top img3")
+      swiper-slide
+        img(src="@/assets/img/top004.jpg" alt="top img4")
+      swiper-slide
+        img(src="@/assets/img/top005.jpg" alt="top img5")
     .title
       .un-title Unsigned2019
       .sub-text 
@@ -24,112 +23,49 @@ body
 </template>
 
 <script>
+import 'swiper/dist/css/swiper.css'
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+
 export default {
-  mounted: function() {
-    window.addEventListener('load', slide);
-    window.addEventListener('scroll', slide);
+  components: {
+    swiper,
+    swiperSlide
   },
-}
-function slide() {
-      const slide = document.querySelectorAll('#slideshow li');
 
-      if(slide.length === 0) {
-        return;
+  data: function() {
+    return {
+      swiperOption: {
+        centeredSlides: true,
+        autoplay: {
+          delay: 2500,
+          disableOnInteraction: false
+        },
+        speed: 3000,
+        effect: 'fade',
+        loop: true,
+        simulateTouch: false,
       }
-
-      [...slide].forEach(slide => {
-        slide.style.backgroundImage = `${slide.getAttribute('src')}`
-        slide.removeAttribute('src');
-      });
-      const totalNum = slide.length - 1;
-      const fadeTime = 2000;
-      const intarvalTime = 5000;
-      let actNum = 0;
-      let nowSlide;
-      let nextSlide;
-
-      slide[0].classList.add('show', 'zoom');
-
-      setInterval(() => {
-        nowSlide = slide[actNum];
-        nextSlide = actNum < totalNum ? slide[++actNum] : slide[actNum = 0];
-
-        nowSlide.classList.remove('show');
-        nextSlide.classList.add('show', 'zoom');
-        setTimeout(() => {
-          nowSlide.classList.remove('zoom');
-        }, fadeTime);
-
-      }, intarvalTime);
     }
+  }
+}
 </script>
 
 <style scoped>
-main {
-  margin: 0;
-  max-width: none;
-  width: 100%;
-  background-color: #2f2f2f;
-}
-
-.top-slideshow-wrapper {
-  height: 100vh;
-  overflow: hidden;
-  position: relative;
-  width: 100%;
-}
-
-.top-slideshow-wrapper .top-logo {
-  bottom: 0;
-  display: block;
-  left: 0;
+.title {
   position: absolute;
-  margin: auto;
-  max-width: 430px;
-  right: 0;
-  top: 65px;
-  width: 83%;
-}
-
-
-.top-slideshow-wrapper #slideshow li {
-  background-position: center;
-  -webkit-background-size: cover;
-  background-size: cover;
-  display: block;
+  width: 100%;
   height: 100%;
-  margin: 0;
-  opacity: 0;
-  position: absolute;
-  -webkit-transition: opacity 2s linear, -webkit-transform 7.5s linear;
-  transition: opacity 2s linear, -webkit-transform 7.5s linear;
-  -o-transition: opacity 2s linear, transform 7.5s linear;
-  transition: opacity 2s linear, transform 7.5s linear;
-  transition: opacity 2s linear, transform 7.5s linear, -webkit-transform 7.5s linear;
-  will-change: opacity, transform;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%,-20%);
+  z-index:2;
+  color: #FFFFFF !important;
 }
 
-.top-slideshow-wrapper #slideshow li.show {
-  opacity: 0.25;
-}
-
-.top-slideshow-wrapper #slideshow li.zoom {
-  -webkit-transform: scale(1.1);
-  -ms-transform: scale(1.1);
-  transform: scale(1.1);
-}
-
-.topimg {
+img {
   width: 100%;
   height: 100vh;
   object-fit: cover;
-}
-
-.title {
-position: absolute;
-top: 30%;
-width: 100%;
-color: #FFFFFF !important; 
 }
 
 .un-title { 
@@ -143,6 +79,22 @@ color: #FFFFFF !important;
 
 .date {
   font-size: 3vw;
+}
+
+/*swiper*/
+@keyframes zoomUp {
+  0% {
+    transform: scale(1);
+  }
+  100% {
+    transform: scale(1.1);
+  }
+}
+
+.swiper-slide-active img,
+.swiper-slide-duplicate-active img,
+.swiper-slide-prev img{
+  animation: zoomUp 10s linear 0s 1 normal both;  
 }
 
 @media screen and (max-width: 1024px) {
